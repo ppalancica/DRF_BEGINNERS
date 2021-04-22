a792e6baa1b92c75b1cd713e0ce027e6fc928122
System Administrator <root@Pavels-MacBook-Pro.local>
2021-04-23 00:13:20 +0300

    Played with permissions

--- drf_beginners/posts/serializers.py
+++ drf_beginners/posts/serializers.py
@@ -6,6 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = (
             'title',
+            'owner',
             'custom_id',
             'category',
             'publish_date',
