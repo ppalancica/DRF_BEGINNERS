909b881b66669af8b41d6624d0783d1df2e4d8c2
System Administrator <root@Pavels-MacBook-Pro.local>
2021-04-22 18:59:54 +0300

    Added put and delete methods

--- drf_beginners/posts/views.py
+++ drf_beginners/posts/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
 from .models import Post
 from .serializers import PostSerializer
 
@@ -20,6 +21,29 @@ class PostView(APIView):
         serializer = PostSerializer(queryset, many=True)
         # if serializer.is_valid():
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # return Response({"message": "The data was saved"})
+            # return Response(serializer.data, status=HTTP_201_CREATED)
+            return Response({"message": "some message"}, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
 
 @csrf_exempt
 def post_list(request):
