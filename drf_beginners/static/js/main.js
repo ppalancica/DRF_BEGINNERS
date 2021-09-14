b01ada0cc1e6909ec6bb1006c23a2e9f3bfa1efd
System Administrator <root@Pavels-MacBook-Pro.local>
2021-09-14 19:49:58 +0300

    Added some basic JavaScript functionality

--- drf_beginners/static/js/main.js
+++ drf_beginners/static/js/main.js
@@ -1,1 +1,56 @@
-console.log("test")
+// console.log("test")
+
+function getPostList() {
+  fetch('/api/posts/')
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data)
+      renderPosts(data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
+
+function renderPosts(data) {
+  return data.map(post => {
+    renderPost(post);
+  })
+}
+
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parrent, el) {
+  return parrent.appendChild(el);
+}
+
+function renderPost(post) {
+  const root = document.getElementById('root');
+  const div = createNode('div');
+  const title = createNode('h2');
+  title.innerText = post.title;
+  append(div, title);
+  append(root, div);
+}
+// function renderPost(post) {
+//   const div = createNode('div');
+//   const title = createNode('h2');
+//   title.innerText = post.title;
+//   append(div, title);
+// }
+// function renderPost(post) {
+//   const div = createNode('div');
+//   const title = createNode('h2');
+//   title.innerText = post.title;
+//   div.appendChild(title);
+// }
+// function renderPost(post) {
+//   const div = document.createElement('div');
+//   const title = document.createElement('h2');
+//   title.innerText = post.title;
+//   div.appendChild(title);
+// }
+
+getPostList()
